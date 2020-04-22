75d93e8b7dde7b047bbfcce0cb741c888f8f3839
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2020-04-22 17:25:14 +0200

    basic structure

--- web-client/src/app/app-routing.module.ts
+++ web-client/src/app/app-routing.module.ts
@@ -1,8 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomepageComponent} from './commons/homepage/homepage.component';
 
-const routes: Routes = [];
+import {GroupListComponent} from './groups/group-list/group-list.component';
+import {GroupAddComponent} from './groups/group-add/group-add.component';
+
+import {MessageListComponent} from './messages/message-list/message-list.component';
+import {MessageAddComponent} from './messages/message-add/message-add.component';
+import {MessageHistoryComponent} from './messages/message-history/message-history.component';
+
+import {ContactListComponent} from './contacts/contact-list/contact-list.component';
+import {ContactAddComponent} from './contacts/contact-add/contact-add.component';
+
+
+const routes: Routes = [
+	{path: '', redirectTo: '/homepage', pathMatch: 'full'},
+	{path: 'homepage', component: HomepageComponent},
+
+	{path: 'groups', redirectTo: 'groups/list'},
+	{path: 'groups/list', component: GroupListComponent},
+	{path: 'groups/add', component: GroupAddComponent},
+
+	{path: 'messages', redirectTo: 'messages/list'},
+	{path: 'messages/list', component: MessageListComponent},
+	{path: 'messages/add', component: MessageAddComponent},
+	{path: 'messages/history', component: MessageHistoryComponent},
+
+	{path: 'contacts', redirectTo: 'contacts/list'},
+	{path: 'contacts/list', component: ContactListComponent},
+	{path: 'contacts/add', component: ContactAddComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+	constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+}
